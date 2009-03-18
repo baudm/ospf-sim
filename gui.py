3c6736592086ecc31bdbb37e03bf82d7dd3c8306
Darwin Bautista <daruuin@gmail.com>
2009-03-18 09:59:14 +0000

    Improved GUI and added more info

--- gui.py
+++ gui.py
@@ -18,9 +18,13 @@ def main():
         configfile = sys.argv[1]
     cfg = SafeConfigParser()
     cfg.read(str(configfile))
-    router = Router(cfg.get('Local', 'hostname'))
+    hostname = cfg.get('Local', 'hostname')
+    w.setWindowTitle(hostname)
+    router = Router(hostname)
+    ifaces = [i for i in cfg.sections() if i.startswith('Local:')]
+    w.interfaces.setRowCount(len(ifaces))
     # Create and configure Router interfaces
-    for iface in [i for i in cfg.sections() if i.startswith('Local:')]:
+    for iface in ifaces:
         # Create
         name = iface.split(':')[1]
         bandwidth = cfg.get(iface, 'bandwidth')
@@ -33,40 +37,33 @@ def main():
         host = cfg.get(link, 'host')
         port = int(cfg.get(link, 'port'))
         router.iface_config(name, address, netmask, host, port)
-
-    iface_count = len(router._interfaces)
-    w.tableWidget_3.setRowCount(iface_count)
-    ifaces = router._interfaces.values()
-    for i in xrange(iface_count):
-        col_count = 0
-        for col in ('name', 'address', 'netmask'):
-            val = getattr(ifaces[i], col)
-            item = QtGui.QTableWidgetItem(str(val))
-            w.tableWidget_3.setItem(i, col_count, item)
-            col_count += 1
+        cols = [name, address, netmask, link]
+        for val in cols:
+            item = QtGui.QTableWidgetItem(val)
+            w.interfaces.setItem(ifaces.index(iface), cols.index(val), item)
 
     def update():
         rows = len(router._table)
-        w.tableWidget.clearContents()
-        w.tableWidget.setRowCount(rows)
+        w.routingTable.clearContents()
+        w.routingTable.setRowCount(rows)
         for i in xrange(rows):
             col_count = 0
             for col in ('dest', 'gateway', 'netmask', 'metric', 'iface'):
                 val = getattr(router._table[i], col)
                 item = QtGui.QTableWidgetItem(str(val))
-                w.tableWidget.setItem(i, col_count, item)
+                w.routingTable.setItem(i, col_count, item)
                 col_count += 1
 
         row_count = 0
-        w.tableWidget_2.clearContents()
+        w.linkStateDb.clearContents()
         for lsa in router._lsdb.values():
             for neighbor, data in lsa.neighbors.iteritems():
-                w.tableWidget_2.setRowCount(row_count + 1)
+                w.linkStateDb.setRowCount(row_count + 1)
                 cost = data[3]
                 col_count = 0
-                for val in (lsa.adv_router, neighbor, cost):
+                for val in (lsa.adv_router, lsa.seq_no, lsa.age, neighbor, cost):
                     item = QtGui.QTableWidgetItem(str(val))
-                    w.tableWidget_2.setItem(row_count, col_count, item)
+                    w.linkStateDb.setItem(row_count, col_count, item)
                     col_count += 1
                 row_count += 1
 
@@ -74,7 +71,6 @@ def main():
     timer.start(1000)
 
     QtCore.QObject.connect(timer, QtCore.SIGNAL('timeout()'), update)
-
     QtCore.QObject.connect(app, QtCore.SIGNAL('lastWindowClosed()'), router.stop)
     w.show()
     t = Thread(target=router.start)
